c7cc9c14679c47067650470aa9227b5eefae7807
Neelesh Pratapwar <npra@camelot-itlab.com>
2016-08-17 10:46:23 +0200

    initial commit

---
+++ bundles/camelot/viz/ext/stackedparallelcolumnchart/camelot_viz_ext_stackedparallelcolumnchart-src/js/propertyeditor/spec.js
@@ -0,0 +1,63 @@
+define("camelot_viz_ext_stackedparallelcolumnchart-src/js/propertyeditor/spec", ["camelot_viz_ext_stackedparallelcolumnchart-src/js/propertyeditor/renderers/checkbox"], function(checkBoxRenderer) {
+	//property editor spec
+	var spec = {
+		"id": "sample.openpe.extension",
+		"dependencies": [],
+		"components": [{
+			"id": "sample.openpe.samplebar.extension",
+			"provide": "sap.viz.controls.propertyeditor.view",
+			"instance": {
+				'charts': ['camelot.viz.ext.stackedparallelcolumnchart'],
+				'view': {
+					'sections': [{
+						"id": "sap.viz.controls.propertyeditor.section.chart_title",
+						"caption": 'EXTEND_CHART_TITLE',
+						"propertyZone": "CHART_TITLE",
+						"groups": [{
+							"id": "sap.viz.controls.propertyeditor.section.chart_title.group.title.visible",
+							"type": "sap.viz.controls.propertyeditor.groupImpl.SwitchGroup",
+							"config": {
+								"property": "title.visible",
+								"label": "Show Chart Title"
+							}
+						}]
+					}, {
+						'id': 'sap.viz.controls.propertyeditor.section.plotArea',
+						'propertyZone': 'PLOTAREA',
+						'caption': 'EXTEND_PLOT_AREA',
+						"groups": [{
+							"id": "sap.viz.controls.propertyeditor.section.plotArea.group.gridline.visible",
+							'renderer': checkBoxRenderer,
+							"config": {
+								"property": "plotArea.gridline.visible",
+								"label": "Show Grid Line"
+							}
+						}, {
+							"id": "sap.viz.controls.propertyeditor.section.plotArea.group.yAxisLine.visible",
+							'renderer': checkBoxRenderer,
+							"config": {
+								"property": "plotArea.yAxisLine.visible",
+								"label": "Show Y-Axis Line"
+							}
+						}, {
+							"id": "sap.viz.controls.propertyeditor.section.plotArea.group.legend.visible",
+							'renderer': checkBoxRenderer,
+							"config": {
+								"property": "plotArea.legend.visible",
+								"label": "Show Legend"
+							}
+						}, {
+							"id": "sap.viz.controls.propertyeditor.section.plotArea.group.yAxisLabel.visible",
+							'renderer': checkBoxRenderer,
+							"config": {
+								"property": "plotArea.yAxisLabel.visible",
+								"label": "Show Y-Axis Label"
+							}
+						}]
+					}]
+				}
+			}
+		}]
+	};
+	return spec;
+});
